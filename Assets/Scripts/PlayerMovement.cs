861bd0c53dd4149e0cf96ad6762187ff46dff134
Nydaxe <james.salehpour@gmail.com>
2025-12-07 16:22:09 -0500

    Basic player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D ridgidbody2D;
+    [SerializeField] float moveSpeed;
+    Vector2 moveInput;
+
+    void OnMovement(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        ridgidbody2D.linearVelocity = moveInput * moveSpeed * Time.deltaTime * 50;
+    }
+}
